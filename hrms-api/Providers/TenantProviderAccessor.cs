b8d689f7867b6284a25cf2cedd61dfff6c277e24
jhoncee <junnelhiro@hiro-c.net>
2026-05-12 13:00:19 +0800

    feat: tenant resolver

--- hrms-api/Providers/TenantProviderAccessor.cs
+++ hrms-api/Providers/TenantProviderAccessor.cs
@@ -18,7 +18,6 @@ public class TenantProviderAccessor : ITenantProvider
             if (_tenantId != Guid.Empty) return _tenantId;
             var context = _httpContextAccessor.HttpContext;
             if (context == null) return Guid.Empty;
-
             // 1. Try to get from Header
             var header = context.Request.Headers["X-Tenant-ID"].FirstOrDefault();
             if (Guid.TryParse(header, out var headerId))
@@ -27,10 +26,10 @@ public class TenantProviderAccessor : ITenantProvider
                 return _tenantId;
             }
             // 2. Fallback: Try to get from JWT Claims
-            var claim = context.User.GetUserId();
-            if (claim == null) return Guid.Empty;
-            _tenantId = claim.Value;
-            return _tenantId;
+            var ClaimTenantId = context.User.GetUserClaim("TenantId");
+            return Guid.TryParse(ClaimTenantId, out var tenantId) && tenantId != Guid.Empty
+                ? tenantId
+                : Guid.Empty;
         }
     }
     public void SetTenantId(Guid tenantId) => _tenantId = tenantId;
