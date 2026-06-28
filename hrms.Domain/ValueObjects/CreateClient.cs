b9e9760389c8aa7d36e45eb180e79cc95740bd61
jhoncee <junnelhiro@hiro-c.net>
2026-06-28 15:02:16 +0800

    fix naming convension

--- hrms.Domain/ValueObjects/CreateClient.cs
+++ hrms.Domain/ValueObjects/CreateClient.cs
@@ -4,7 +4,7 @@ public class CreateClient
 {
     public string Code { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
-    public string Status { get; set; }
+    public string Status { get; set; } = "Active";
 
 }
 
