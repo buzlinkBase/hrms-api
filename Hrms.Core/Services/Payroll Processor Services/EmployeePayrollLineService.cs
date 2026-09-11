7ab692e8f66e4d50d0e38ecc0ea8da748e83a16a
jhoncee <junnel@hiro-c.net>
2026-09-11 19:08:46 +0800

    remove capping

--- Hrms.Core/Services/Payroll Processor Services/EmployeePayrollLineService.cs
+++ Hrms.Core/Services/Payroll Processor Services/EmployeePayrollLineService.cs
@@ -202,7 +202,7 @@ public class EmployeePayrollLineService
         payrollLine.NightDifferentialOTPay = employeeBasicCalc.Sum(x => x.TotalNDOT);
         payrollLine.OTPremiumPay = employeeBasicCalc.Sum(x => x.OTPremiumPay);
         payrollLine.NDPremiumPay = employeeBasicCalc.Sum(x => x.NDPremiumPay);
-        payrollLine.AbsencesAmount = Math.Min(employeeBasicCalc.Sum(x => x.AbsentAmount), payrollLine.BasicPay);
+        payrollLine.AbsencesAmount = employeeBasicCalc.Sum(x => x.AbsentAmount);
 
         payrollLine.RegularDayPay = employeeBasicCalc.Sum(x => x.RegularDayPay);
         payrollLine.RegularOTPay = employeeBasicCalc.Sum(x => x.RegularOTPay);
