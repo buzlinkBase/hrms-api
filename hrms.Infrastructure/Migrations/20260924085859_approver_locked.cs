aba7130482b372def352ca3b926b3a795e6894fe
jhoncee <junnel@hiro-c.net>
2026-09-24 17:06:30 +0800

    locked

---
+++ hrms.Infrastructure/Migrations/20260924085859_approver_locked.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hrms.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class approver_locked : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CycleNumber",
+                table: "ApprovalInstances",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CycleNumber",
+                table: "ApprovalActions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CycleNumber",
+                table: "ApprovalInstances");
+
+            migrationBuilder.DropColumn(
+                name: "CycleNumber",
+                table: "ApprovalActions");
+        }
+    }
+}
